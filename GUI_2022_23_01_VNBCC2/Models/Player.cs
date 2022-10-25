8006e0a51c5d957272a0c012fcdd6b1531a38ef8
halajbalazs <halajbali10@gmail.com>
2022-10-25 09:04:02 +0200

    Create Menu window in MainWindow and created Player model.

---
+++ GUI_2022_23_01_VNBCC2/Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_2022_23_01_VNBCC2.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }  //calculated from time, created food and reach level
+        public DateTime Time { get; set; }  //??
+        public int Level { get; set; }  //reached level
+
+        public Player(string name)
+        {
+            this.Name = name;
+        }
+    }
+}

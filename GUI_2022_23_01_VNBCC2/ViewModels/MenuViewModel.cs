84f7f98c48414f4dc0543148cc672536a9238f19
halajbalazs <halajbali10@gmail.com>
2022-10-25 09:09:42 +0200

    Created MenuViewModel for Menu window and bind Players list.

---
+++ GUI_2022_23_01_VNBCC2/ViewModels/MenuViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GUI_2022_23_01_VNBCC2.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_2022_23_01_VNBCC2.ViewModels
+{
+    public class MenuViewModel
+    {
+       public BindingList<Player> Players { get; set; }
+    }
+}

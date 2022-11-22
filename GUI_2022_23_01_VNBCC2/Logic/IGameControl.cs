ac725cf5fe7ab4d0d8d117139397a43fe0804852
gergely.kartal.nagy@stud.uni-obuda.hu <gergely.kartal.nagy@stud.uni-obuda.hu>
2022-11-22 10:22:43 +0100

    Levels' Logic part done

---
+++ GUI_2022_23_01_VNBCC2/Logic/IGameControl.cs
@@ -0,0 +1,6 @@
+﻿namespace GUI_2022_23_01_VNBCC2.Logic
+{
+    internal interface IGameControl
+    {
+    }
+}
---
+++ GUI_2022_23_01_VNBCC2/Logic/IGameModel.cs
@@ -0,0 +1,9 @@
+﻿using static GUI_2022_23_01_VNBCC2.Logic.GameLogic;
+
+namespace GUI_2022_23_01_VNBCC2.Logic
+{
+    public interface IGameModel
+    {
+        Items[,] GameMatrix { get; set; }
+    }
+}
